d68f0447b89fd480f19b871fdc0b3397caeffcd3
root <root@docker.pve1>
2025-12-29 12:20:27 -0300

    feat: inclusão do bot token

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
+BOT_TOKEN = os.getenv('BOT_TOKEN') 
 DESTINO_RAW = os.getenv('DESTINO')
 SESSION_PATH = '/app/data/monitor_session'
 
